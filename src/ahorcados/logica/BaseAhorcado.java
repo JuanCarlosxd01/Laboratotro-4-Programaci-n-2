4b337f5dc458761b7dc40c751c0d1b0eef5824b1
diegoyou623 <diegoyou623@gmail.com>
2026-08-15 09:08:33 -0600

    Base del juego

--- src/ahorcados/logica/BaseAhorcado.java
+++ src/ahorcados/logica/BaseAhorcado.java
@@ -4,10 +4,75 @@
  */
 package ahorcados.logica;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author diego
  */
-public class BaseAhorcado {
+    public abstract class BaseAhorcado implements AhorcadoJuego {
+
+    protected String palabraSecreta;
     
-}
+    protected String palabraMostrada;
+    protected ArrayList <Character> letrasIngresadas;
+    protected int intentosRestantes;
+    protected final int maximoIntentos = 6;
+    
+    protected ArrayList <String> dibujoAhorcado;
+
+    public BaseAhorcado() {
+        intentosRestantes = maximoIntentos;
+        
+        letrasIngresadas = new ArrayList<>();
+        dibujoAhorcado = new ArrayList<>();
+
+        for (int i = 0; i < 6; i++) {
+            dibujoAhorcado.add(" ");
+        }
+    }
+
+    @Override
+    public void establecerPalabraSecreta(String palabra) {
+        palabraSecreta = palabra.toUpperCase();
+
+        palabraMostrada = "";
+
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+            palabraMostrada += "_";
+        }
+    }
+
+    public abstract void UpdaterPalabraMostrada(char letra);
+
+    public abstract boolean verificarLetra(char letra);
+    
+
+    public abstract boolean verificarVictoria();
+
+    public boolean verificarDerrota() {
+        return intentosRestantes <= 0;
+    }
+
+    public String getPalabraMostrada() {
+        return palabraMostrada;
+    }
+
+    public int getIntentosRestantes() {
+        
+        return intentosRestantes;
+        
+    }
+
+    public ArrayList<Character> getLetrasIngresadas() {
+        
+        return letrasIngresadas;
+    }
+
+    public String getPalabraSecreta() {
+        
+        return palabraSecreta;
+    }
+    
+    
+    }
