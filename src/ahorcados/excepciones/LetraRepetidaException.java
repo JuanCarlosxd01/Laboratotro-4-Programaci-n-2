44cf79ab4bab06a8b473d0d8035ed82ddc03f3be
Jhov12 <hossycarias@gmail.com>
2026-08-15 09:21:24 -0600

    Implement LetraRepetidaException class extending Exception

--- src/ahorcados/excepciones/LetraRepetidaException.java
+++ src/ahorcados/excepciones/LetraRepetidaException.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+ 
 package ahorcados.excepciones;
 
-/**
- *
- * @author diego
- */
-public class LetraRepetidaException {
-    
+public class LetraRepetidaException extends Exception {
+
+    public LetraRepetidaException(String mensaje) {
+        super(mensaje);
+    }
 }
