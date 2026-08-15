8df617868ba1badab08c81af807cd788ec6908cf
Jhov12 <hossycarias@gmail.com>
2026-08-15 09:47:04 -0600

    Update GestorDePalabras.java

--- src/ahorcados/logica/GestorDePalabras.java
+++ src/ahorcados/logica/GestorDePalabras.java
@@ -2,12 +2,52 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ahorcados.logica;
+package ahorcados.logica;//Jhoveth Moncada
+import java.util.Random;
 
-/**
- *
- * @author diego
- */
+import java.util.ArrayList;
+
+import ahorcados.excepciones.PalabraDuplicadaException;
+
 public class GestorDePalabras {
-    
+
+    private ArrayList<String> palabras;
+    private Random random;
+
+    public GestorDePalabras() {
+        palabras = new ArrayList<>();
+        random = new Random();
+    }
+
+    public void agregarPalabra(String palabra) throws PalabraDuplicadaException {
+
+        if (palabra == null || palabra.isBlank()) {
+            return;
+        }
+
+        palabra = palabra.toUpperCase();
+
+        if (palabras.contains(palabra)) {
+            throw new PalabraDuplicadaException("La palabra que desea ingresar, ya existe estimado");
+        }
+
+        palabras.add(palabra);
+    }
+
+    public String obtenerPalabra() {
+
+        if (palabras.isEmpty()) {
+            return null;
+        }
+
+        int posicion = random.nextInt(palabras.size());
+
+        return palabras.get(posicion);
+    }
+
+    public int cantidadPalabras() {
+        return palabras.size();
+    }
 }
+    
+
