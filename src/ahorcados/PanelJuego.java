fd14701d8ac578fc1b968399b8a14e645594bff9
JuanCarlosxd01 <juan_alcerro@unitec.edu>
2026-08-15 08:41:23 -0600

    Avance interfaz

--- src/ahorcados/PanelJuego.java
+++ src/ahorcados/PanelJuego.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ahorcados;
 
-/**
- *
- * @author nosoy
- */
+import javax.swing.*;
+import java.awt.*;
+
 public class PanelJuego {
+    private CardLayout transicion;
+    private JPanel contenedor;
     
+    public PanelJuego(JPanel contenedor, CardLayout transicion){
+        
+    }
 }
--- src/ahorcados/PanelMenu.java
+++ src/ahorcados/PanelMenu.java
@@ -1,13 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ahorcados;
 
-/**
- *
- * @author nosoy
- */
-public class PanelMenu {
+import javax.swing.*;
+import java.awt.*;
+
+public class PanelMenu extends JPanel{
     
+    private CardLayout transicion;
+    private JPanel contenedor;
+    
+    public PanelMenu(JPanel contenedor, CardLayout transicion){
+        
+    }
 }
