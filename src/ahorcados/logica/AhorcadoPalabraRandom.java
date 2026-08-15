7ac4f9d049b7ba9b7acfe269703d702013549dc9
diegoyou623 <diegoyou623@gmail.com>
2026-08-15 09:45:40 -0600

    logica de palabras

--- src/ahorcados/logica/AhorcadoPalabraRandom.java
+++ src/ahorcados/logica/AhorcadoPalabraRandom.java
@@ -3,11 +3,70 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package ahorcados.logica;
-
+import ahorcados.excepciones.LetraInvalidaException;
+import ahorcados.excepciones.LetraRepetidaException;
 /**
  *
  * @author diego
  */
-public class AhorcadoPalabraRandom {
-    
+public class AhorcadoPalabraRandom extends BaseAhorcado {
+
+    private GestorDePalabras gestor;
+
+    public AhorcadoPalabraRandom(GestorDePalabras gestor) {
+        this.gestor = gestor;
+        establecerPalabraSecreta(gestor.obtenerPalabraAleatoria());
+    }
+
+    @Override
+    public void jugar(char letra) throws Exception {
+
+        letra = Character.toUpperCase(letra);
+
+        if (!Character.isLetter(letra)) {
+            throw new LetraInvalidaException("Letra ingresad no es valida, intenta nuevamente");
+        }
+
+        if (letrasIngresadas.contains(letra)) {
+            throw new LetraRepetidaException("Esta letra ya fue elegida, intenta nuevamente");
+        }
+
+        letrasIngresadas.add(letra);
+
+        if (verificarLetra(letra)) {
+            UpdatePalabraMostrada(letra);
+        } else 
+        {
+            intentosRestantes--;
+        }
+    }
+
+    @Override
+    public boolean verificarLetra(char letra) {
+        return palabraSecreta.indexOf(letra) >= 0;
+    }
+
+    @Override
+    public void UpdatePalabraMostrada(char letra) {
+
+        String nuevaPalabra = " ";
+
+        for (int i = 0; i < palabraSecreta.length(); i++) {
+
+            if (palabraSecreta.charAt(i) == letra) {
+                nuevaPalabra += letra;
+            } 
+            else
+            {
+                nuevaPalabra += palabraMostrada.charAt(i);
+            }
+        }
+
+        palabraMostrada = nuevaPalabra;
+    }
+
+    @Override
+    public boolean verificarVictoria() {
+        return palabraMostrada.equals(palabraSecreta);
+    }
 }
